8fdb90af874e7b62cc8e756c673551f97b00ba38
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-03-17 12:34:52 -0300

    session01 SOLID

---
+++ PrincipiosSOLIDePOO/Session01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Session01
+{
+    class Program
+    {
+        static int Somar(int x, int z = 0, int a = 0)
+        {
+            return x + z + a;
+        }
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine(Somar(x: 2, a: 10, z: 34));
+        }
+    }
+}
