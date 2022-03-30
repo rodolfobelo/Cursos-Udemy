29b929f9f1e5abe962d35d077ea705e82a4bc65b
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-03-29 22:24:49 -0300

    Repositories - Usuario

---
+++ eCommerce.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+
+{
+    public interface IUsuarioRepository
+    {
+        public List<Usuario> GetUsuarios();
+        public Usuario GetUsuario(int id);
+        public void InsertUsuario(Usuario usuario);
+        public void UpdateUsuario(Usuario usuario);
+        public void DeleteUsuario(int id);
+
+    }
+}
---
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,36 @@
+﻿using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario() { Id = 1, Nome="Filipe", Email="filipe@gmail.com"},
+            new Usuario() { Id = 2, Nome = "Marcelo", Email = "marcelo@gmail.com"},
+            new Usuario() { Id = 2, Nome = "Jessica", Email = "jessica@gmail.com"}
+        };
+
+        public List<Usuario> GetUsuarios()
+        {
+            return _db;
+        }
+        public Usuario GetUsuario(int id)
+        {
+            return _db.FirstOrDefault(a => a.Id == id);
+        }
+        public void InsertUsuario(Usuario usuario)
+        {
+            _db.Add(usuario);
+        }
+        public void UpdateUsuario(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(a => a.Id == usuario.Id));
+            _db.Add(usuario);
+        }
+        public void DeleteUsuario(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(a => a.Id == id));
+        }
+    }
+}
