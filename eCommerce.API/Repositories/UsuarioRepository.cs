06705a23e697b767aee5c6c028ab6309370614f7
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-04-18 11:59:13 -0300

    Pegar usuario - Contatos

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -56,7 +56,7 @@ namespace eCommerce.API.Repositories
             try
             {
                 SqlCommand command = new SqlCommand();
-                command.CommandText = "SELECT * FROM Usuarios WHERE Id = @Id";
+                command.CommandText = "SELECT * FROM Usuarios u LEFT JOIN Contatos c ON c.UsuarioId = u.Id WHERE u.Id = @Id";
                 command.Parameters.AddWithValue("@Id", id);
                 command.Connection = (SqlConnection)_connection;
 
@@ -66,7 +66,7 @@ namespace eCommerce.API.Repositories
                 while (dataReader.Read())
                 {
                     Usuario usuario = new Usuario();
-                    usuario.Id = dataReader.GetInt32("Id");
+                    usuario.Id = dataReader.GetInt32(0);
                     usuario.Nome = dataReader.GetString("Nome");
                     usuario.Email = dataReader.GetString("Email");
                     usuario.Sexo = dataReader.GetString("Sexo");
@@ -75,6 +75,13 @@ namespace eCommerce.API.Repositories
                     usuario.NomeMae = dataReader.GetString("NomeMae");
                     usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
                     usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                    Contato contato = new Contato();
+                    contato.Id = dataReader.GetInt32(9);
+                    contato.UsuarioId = usuario.Id;
+                    contato.Telefone = dataReader.GetString("Telefone");
+                    contato.Celular = dataReader.GetString("Celular");
+                    usuario.Contato = contato;
 
                     return usuario;
                 }
