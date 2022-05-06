cfb98ee75e29eff28480386905d598c61987b095
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-05-06 09:14:51 -0300

    Conexao de BD

--- eCommerce.API/Repositories/ConexaoBD.cs
+++ eCommerce.API/Repositories/ConexaoBD.cs
@@ -13,5 +13,10 @@ namespace eCommerce.API.Repositories
             string c = @"Server=localhost;Database=eCommerce;User Id=sa;Password=!Q2w3e$R;";
             return c;
         }
+        public string localEXEMPLO_12134()
+        {
+            string c = @"Server=localhost;Database=EXEMPLO_12134;User Id=sa;Password=!Q2w3e$R;";
+            return c;
+        }
     }
 }
