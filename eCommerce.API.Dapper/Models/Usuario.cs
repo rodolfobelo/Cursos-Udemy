43704f0087a4d95c0a711f25e5942632fe48e736
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-05-19 21:56:34 -0300

    Relacionamentos BD nas classes

--- eCommerce.API.Dapper/Models/Usuario.cs
+++ eCommerce.API.Dapper/Models/Usuario.cs
@@ -11,6 +11,9 @@
         public string NomeMae { get; set; }
         public string SituacaoCadastro { get; set; }
         public DateTimeOffset DataCadastro { get; set; }
+        public Contato Contato { get; set; }
+        public ICollection<EnderecoEntrega> EnderecoEntrega { get; set; }
+        public ICollection<Departamento> Departamentos { get; set; }
 
     }
 }
