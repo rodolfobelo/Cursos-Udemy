4ab73a5537f1f0443fb9b11517cf44d6fc60959c
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-10-31 21:59:23 -0300

    Classe Database

---
+++ Cod3r/php7Completo/innout/public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/Database.php');
+
+Database::getConnection();
---
+++ Cod3r/php7Completo/innout/src/config/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database
+{
+    public static function getConnection()
+    {
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPath);
+        $conn = new mysqli(
+            $env['host'],
+            $env['username'],
+            $env['password'],
+            $env['database']
+        );
+
+        if ($conn->connect_error) {
+            die("Erro: " . $conn->connect_error);
+            echo 'aqui';
+        }
+
+        return $conn;
+    }
+
+    public function teste(){
+        return 'teste';
+    }
+}
