67d302801ee2e73f32c8109df592d8e39902fa9c
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-11-07 21:50:10 -0300

    Begin in Router, Controllers e Views

--- DevWebLaravelPHPVueJS/app_super_gestao/routes/web.php
+++ DevWebLaravelPHPVueJS/app_super_gestao/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
+
+Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class,'sobreNos']);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem){
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
 });
