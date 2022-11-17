5a781a7e5996e3f3c97a534908cfb803c5262858
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2022-11-16 21:45:39 -0300

    Comando for blade

--- DevWebLaravelPHPVueJS/app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ DevWebLaravelPHPVueJS/app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -6,7 +6,31 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index(){
-        return view('app.fornecedor.index');
+    public function index() {
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00',
+                'ddd' => 85,
+                'telCelular' => 987654321
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => '765.543.0001/95-87',
+                'ddd' => 85,
+                'telCelular' => 912456789
+            ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => '712.123.0001/43-123',
+                'ddd' => 85,
+                'telCelular' => 912456789
+            ]
+        ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- DevWebLaravelPHPVueJS/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ DevWebLaravelPHPVueJS/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,29 @@
-<h1>fornecedores app</h1>
+<h1>Fornecedor</h1>
+
+@php
+    /*
+    if(empty($variavel)) {} //retornar true se a variável estiver vazia
+    - ''
+    - 0
+    - 0.0
+    - '0'
+    - null
+    - false
+    - array()
+    - $var
+    
+    $posicao = 1;*/
+@endphp
+    @isset($fornecedores)
+        @for($i = 0; isset($fornecedores[$i]);$i++)
+            Fornecedor: {{ $fornecedores[$i]['nome'] }}
+            <br>
+            Status: {{ $fornecedores[$i]['status'] }}
+            <br>
+            CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não preenchido' }}
+            <br>
+            Telefone Celular: ({{ $fornecedores[$i]['ddd'] ?? 'ddd não informado!' }})
+             {{ $fornecedores[$i]['telCelular'] ?? 'ddd não informado!' }}
+            <hr>
+        @endfor    
+    @endisset
