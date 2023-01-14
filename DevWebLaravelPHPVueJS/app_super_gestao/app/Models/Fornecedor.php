13d5af81d94b621466c3bb95d8ad535aa405164f
= <=>
2023-01-14 14:35:30 -0300

    SoftDeletes - Cls Forncecedor

--- DevWebLaravelPHPVueJS/app_super_gestao/app/Models/Fornecedor.php
+++ DevWebLaravelPHPVueJS/app_super_gestao/app/Models/Fornecedor.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    use SoftDeletes;
     protected $table = 'fornecedores';
-    protected $fillable = ['nome', 'site', 'uf', 'email', 'ativo'];
+    protected $fillable = ['nome', 'site', 'uf', 'email', 'ativo'];    
 }
