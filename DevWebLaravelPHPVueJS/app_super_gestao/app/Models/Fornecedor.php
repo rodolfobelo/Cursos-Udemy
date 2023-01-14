1f7a48905ded2c6786c352018fd7cde68da200dc
= <=>
2023-01-14 14:41:01 -0300

    t

--- DevWebLaravelPHPVueJS/app_super_gestao/app/Models/Fornecedor.php
+++ DevWebLaravelPHPVueJS/app_super_gestao/app/Models/Fornecedor.php
@@ -10,5 +10,6 @@ class Fornecedor extends Model
 {
     use SoftDeletes;
     protected $table = 'fornecedores';
-    protected $fillable = ['nome', 'site', 'uf', 'email', 'ativo'];    
+    protected $fillable = ['nome', 'site', 'uf', 'email', 'ativo'];
+        
 }
