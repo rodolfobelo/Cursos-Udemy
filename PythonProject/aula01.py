709ce700d27b40c370465bcdbbeccd5e8c2ffee7
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2025-01-07 10:23:44 -0300

    python

---
+++ PythonProject/aula01.py
@@ -0,0 +1,8 @@
+from operator import concat
+teste = '20'
+teste01 = '25'
+mensagem = 'Este curso é muito bom!'
+print('Feliz', teste + teste01)
+
+print(mensagem.replace('bom', 'excelente'))
+print(mensagem.split()[0])
